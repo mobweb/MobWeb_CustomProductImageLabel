a04eb57673b85c2034022930d0c96433729812c8
Louis Bataillard <info@mobweb.ch>
2014-05-23 19:11:58 +0200

    fixed previous commit

--- app/code/local/MobWeb/CustomProductImageLabel/Block/Product/List.php
+++ app/code/local/MobWeb/CustomProductImageLabel/Block/Product/List.php
@@ -2,8 +2,8 @@
 
 class MobWeb_CustomProductImageLabel_Block_Product_List extends Mage_Catalog_Block_Product_List
 {
-	public function getImageLabel()
+	public function getImageLabel($product)
 	{
-		return Mage::helper('customproductimagelabel/data')->getCustomProductImageLabel($this->getProduct());
+		return Mage::helper('customproductimagelabel/data')->getCustomProductImageLabel($product);
 	}
 }
